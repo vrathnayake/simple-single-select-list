68f5c13ccec184ae3597fc32310ad37676782a65
vindi <vrathnayake@gmail.com>
2019-07-04 10:03:23 -0700

    Initial

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'simple-single-select-list'
+import SimpleSSList from 'simple-single-select-list'
 
+const list = [
+  {id:1, value:"option 1"},
+  {id:2, value:"option 2"},
+  {id:3, value:"option 3"}
+]
 export default class App extends Component {
+
+
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <SimpleSSList list={list} />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,20 +3,40 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
+export default class SimpleSingleSelectList extends Component {
+
+  itemSelected =()=>{
+    alert('item clicked. Bind onItemSelected to a method handler ')
+}
   static propTypes = {
     text: PropTypes.string
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
+    const styleClasses = [styles.SelectOption]
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className={styles.SelectContainer} >
+          {this.props.list.map((item) => {
+              if(item.value === ( this.props.defaultvalue)){
+                  styleClasses.push(styles.Selected)
+              } else {
+                  if(styleClasses.length > 1){
+                      styleClasses.pop()
+                  }
+                  
+              }
+              return (
+                  
+                  <div
+                      className={styleClasses.join(' ')}
+                      onClick={() => (typeof(this.props.onItemSelected) !== 'undefined') ? this.props.divclicked(item.value) : this.itemSelected()}
+                      key={item.id} >
+                      {item.value}
+                  </div>
+              )
+          }
+          )}
       </div>
-    )
+  )
   }
 }
